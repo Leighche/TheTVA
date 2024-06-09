192cef05bc7adc0c75f233007dd667d9c3135e93
ST10083666 <st10083666@vcconnect.edu.za>
2024-06-09 16:08:27 +0200

    GraphFix
    Added new code to to display graphs in the catergoryGraph. Added Firebase storage code and new dependincies.

--- MissMinutes/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/missminutes/databinding/ActivityCategoryGraphBinding.java
+++ MissMinutes/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/missminutes/databinding/ActivityCategoryGraphBinding.java
@@ -11,7 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
 import androidx.viewbinding.ViewBindings;
 import com.example.missminutes.R;
-import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import java.lang.NullPointerException;
 import java.lang.Override;
 import java.lang.String;
@@ -21,7 +21,7 @@ public final class ActivityCategoryGraphBinding implements ViewBinding {
   private final RelativeLayout rootView;
 
   @NonNull
-  public final BarChart idBarChart;
+  public final LineChart idBarChart;
 
   @NonNull
   public final TextView idTVHead;
@@ -30,7 +30,7 @@ public final class ActivityCategoryGraphBinding implements ViewBinding {
   public final RelativeLayout main;
 
   private ActivityCategoryGraphBinding(@NonNull RelativeLayout rootView,
-      @NonNull BarChart idBarChart, @NonNull TextView idTVHead, @NonNull RelativeLayout main) {
+      @NonNull LineChart idBarChart, @NonNull TextView idTVHead, @NonNull RelativeLayout main) {
     this.rootView = rootView;
     this.idBarChart = idBarChart;
     this.idTVHead = idTVHead;
@@ -65,7 +65,7 @@ public final class ActivityCategoryGraphBinding implements ViewBinding {
     int id;
     missingId: {
       id = R.id.idBarChart;
-      BarChart idBarChart = ViewBindings.findChildViewById(rootView, id);
+      LineChart idBarChart = ViewBindings.findChildViewById(rootView, id);
       if (idBarChart == null) {
         break missingId;
       }
--- MissMinutes/app/src/main/java/com/example/missminutes/CategoryGraph.kt
+++ MissMinutes/app/src/main/java/com/example/missminutes/CategoryGraph.kt
@@ -5,9 +5,13 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.LegendEntry
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.EventListener
@@ -15,59 +19,25 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
 
+
 class CategoryGraph : AppCompatActivity() {
 
     lateinit var barChart: BarChart
     lateinit var barData: BarData
     lateinit var barDataSet: BarDataSet
     private lateinit var categoryArrayList: ArrayList<CategorySummary>
-    private lateinit var categoryHoursMap: HashMap<String, Double>
+   // private lateinit var categoryHoursMap: HashMap<String, Double>
     private lateinit var myCategoryAdapter: MyCategoryAdapter
     private lateinit var db: FirebaseFirestore
     private lateinit var lineChart: LineChart
 
 
-    lateinit var barEntriesList: ArrayList<BarEntry>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_graph)
 
 
-
-        barChart = findViewById(R.id.idBarChart)
-
-        // on below line we are calling get bar
-        // chart data to add data to our array list
-        getBarChartData()
-
-        // on below line we are initializing our bar data set
-        barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
-
-        // on below line we are initializing our bar data
-        barData = BarData(barDataSet)
-
-        // on below line we are setting data to our bar chart
-        barChart.data = barData
-
-        // on below line we are setting colors for our bar chart text
-        //barDataSet.valueTextColor = @c
-
-        // on below line we are setting color for our bar data set
-       // barDataSet.setColor(resources.getColor(R.color.purple_200))
-
-        // on below line we are setting text size
-        barDataSet.valueTextSize = 16f
-
-        // on below line we are enabling description as false
-        barChart.description.isEnabled = false
-
-
-
-
-    }//end
-
-    private fun getBarChartData() {
 
         //THis method retrieves the data from firesotre
 
@@ -88,10 +58,9 @@ class CategoryGraph : AppCompatActivity() {
                         Log.e("Firestore error", error.message.toString())
                         return
                     }
-// Clear the existing data
-                    categoryArrayList.clear()
-                    // Clear the category-hours map
-                    categoryHoursMap.clear()
+                    var x=0
+                    val entries = mutableListOf<Entry>()
+
 //This is a foreach loop that handles the retrieving of info, and handles errors in case of any values being null or empty.
                     value?.documentChanges?.forEach { dc ->
                         if (dc.type == DocumentChange.Type.ADDED) {
@@ -100,10 +69,10 @@ class CategoryGraph : AppCompatActivity() {
                             val category =
                                 task.categoryName ?: "Unknown" // Default to "Unknown" if null
                             val hours = task.hours?.toDouble() ?: 0.0 // Default to 0.0 if null
-
+                            entries.add(Entry(x.toFloat(),hours.toFloat()))
                             // Accumulate hours for each category
-                            categoryHoursMap[category] =
-                                categoryHoursMap.getOrDefault(category, 0.0) + hours
+                           // categoryHoursMap[category] =
+                          //      categoryHoursMap.getOrDefault(category, 0.0) + hours
                         }
                     }
 
@@ -112,15 +81,21 @@ class CategoryGraph : AppCompatActivity() {
                     // CategorySummary(categoryName, totalHours).categoryName
                     //}.toMutableList()
 
-                    val categoryNames = categoryHoursMap.map { (categoryName, totalHours) ->
-                        CategorySummary(categoryName, totalHours).categoryName
-                    }
+                   // val categoryNames = categoryHoursMap.map { (categoryName, totalHours) ->
+                   //     CategorySummary(categoryName, totalHours).categoryName
+                  //  }
 
-                    val totalHours = categoryHoursMap.map { (categoryName, totalHours) ->
-                        CategorySummary(categoryName, totalHours).totalHours.toFloat()
-                    }
+                   // val totalHours = categoryHoursMap.map { (categoryName, totalHours) ->
+                   //     CategorySummary(categoryName, totalHours).totalHours.toFloat()
+                    //}
 
 
+
+
+                    //while(x < categoryHoursMap.count()){
+                    //entries.add(Entry(x.toFloat(),totalHours[x]))
+                    //x++
+                    //}
 
                     // Update the adapter with the list of unique categories
                     //myCategoryAdapter.updateData(uniqueCategoriesList)
@@ -128,10 +103,13 @@ class CategoryGraph : AppCompatActivity() {
                     // Update the adapter with the new data
                     //myCategoryAdapter.notifyDataSetChanged()
 
-                    barEntriesList = ArrayList()
-                    totalHours.forEach{th ->
-                        barEntriesList.add(BarEntry(1f, th))
-                    }
+
+
+                    val dataSet = LineDataSet(entries, "Data")
+                    //populate line chart with data
+                    lineChart.data = LineData(dataSet)
+                    //sets the chart on screen
+                    lineChart.invalidate()
 
 
 
